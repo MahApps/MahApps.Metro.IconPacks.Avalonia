33f3d7f90a16b77fb85a76c56e4d7427b0abd7a1
punker76 <punkerat76@gmail.com>
2024-11-07 16:33:42 +0100

    refactor

--- src/IconPacks.Avalonia.Core/Properties/AssemblyInfo.cs
+++ src/IconPacks.Avalonia.Core/Properties/AssemblyInfo.cs
@@ -1,9 +1,6 @@
-﻿using System.Runtime.InteropServices;
-using Avalonia.Metadata;
+﻿using Avalonia.Metadata;
 
 [assembly: XmlnsPrefix("urn:iconpacks-avalonia", "iconPacks")]
 [assembly: XmlnsDefinition("urn:iconpacks-avalonia", "IconPacks")]
 [assembly: XmlnsDefinition("urn:iconpacks-avalonia", "IconPacks.Avalonia")]
 [assembly: XmlnsDefinition("urn:iconpacks-avalonia", "IconPacks.Avalonia.Attributes")]
-
-[assembly: ComVisible(false)]
